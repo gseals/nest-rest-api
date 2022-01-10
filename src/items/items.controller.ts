04e5df4da4139860fa4d6d3228be16cc750609fc
Gabriel Seals <sealsgj@gmail.com>
2022-01-10 16:56:04 -0600

    finished setting up DTO. beginning express

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get, Post, Put, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Body } from '@nestjs/common';
+import { CreateItemDto } from './dto/create-item.dto';
 
 @Controller('items')
 export class ItemsController {
@@ -8,7 +9,7 @@ export class ItemsController {
   }
 
   @Post()
-  create(): string {
-    return 'Create item';
+  create(@Body() createItemDto: CreateItemDto): string {
+    return `Name: ${createItemDto.name} Desc: ${createItemDto.description}`;
   }
 }
