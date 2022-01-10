7eb3c23e01acd66756d7fcd23a63f01942e1c0b2
Gabriel Seals <sealsgj@gmail.com>
2022-01-09 19:16:09 -0600

    beginning DTO section

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete } from '@nestjs/common';
 
 @Controller('items')
-export class ItemsController {}
+export class ItemsController {
+  @Get()
+  findAll(): string {
+    return 'Get all items';
+  }
+
+  @Post()
+  create(): string {
+    return 'Create item';
+  }
+}
