1aa548381231cbef7aa09171b6c85b9030b51c93
Gabriel Seals <sealsgj@gmail.com>
2022-01-11 22:32:09 -0600

    adding express example

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,11 +1,23 @@
-import { Controller, Get, Post, Put, Delete, Body } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Body,
+  Req,
+  Res,
+} from '@nestjs/common';
+import { ExpressAdapter } from '@nestjs/platform-express';
 import { CreateItemDto } from './dto/create-item.dto';
+import { Request, Response } from 'express';
 
 @Controller('items')
 export class ItemsController {
   @Get()
-  findAll(): string {
-    return 'Get all items';
+  findAll(@Req() req: Request, @Res() res: Response): Response {
+    console.log(req.url);
+    return res.send('Hello world');
   }
 
   @Post()
